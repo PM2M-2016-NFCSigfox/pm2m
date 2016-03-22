8738bac0c40b0a941730a4c3334999136737659e
aitmoula <a.aitmouloud@outlook.com>
2016-03-22 16:44:55 +0100

    Dup case

--- rondes/routes/api.js
+++ rondes/routes/api.js
@@ -5,15 +5,18 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    function display_error(errP) {
+    function display_error(errP, connection) {
         console.log('Error during processing: \n\t', errP);
         res.render('api', {
             title: 'SigFox Callback API Error',
             id: idP,
             data: dataP,
-            time: timeP.toLocaleDateString(),
+            time: timeP.toLocaleDateString() + ' ('+recievedTime+')',
             error: errP
         });
+        if (typeof connection !== 'undefined' && connection) {
+            connection.end();
+        }
     }
 
     var idP = req.query.id;
@@ -21,6 +24,8 @@ router.get('/', function(req, res, next) {
     var timeP = new Date(0); // The 0 there is the key, which sets the date to the epoch
     timeP.setUTCSeconds(req.query.time);
     timeP.setHours(0, 0, 0, 0);
+
+    var recievedTime = req.query.time;
 
     req.query.time;
     var err = null;
@@ -41,59 +46,69 @@ router.get('/', function(req, res, next) {
             password: config.password,
             database: config.db
         });
-try {
+
         connection.connect();
         // Vérifier que l'id_tag est affecté à un garde
         connection.query('SELECT g.id_garde FROM garde g WHERE g.id_tag = ?', [dataP], function(err, rows, fields) {
-            if (err) throw err;
-            if (!rows || rows.length == 0) {
-                display_error("No garde with for tag id : " + dataP);
-            } else {
-                console.log('Got Garde: ', rows[0].id_garde);
-                var garde_id = rows[0].id_garde;
+            if (err) display_error(err, connection);
+            else {
+                if (!rows || rows.length == 0) {
+                    display_error("No garde with for tag id : " + dataP);
+                } else {
+                    console.log('Got Garde: ', rows[0].id_garde);
+                    var garde_id = rows[0].id_garde;
 
-                // Vérifier que l'id du PDC existe.
-                connection.query('SELECT pdc.id_pdc, pdc.id_ronde FROM pointDeControle pdc WHERE pdc.id_pdc = ?', [idP], function(err, rows, fields) {
-                    if (err) throw err;
-                    if (!rows || rows.length == 0) {
-                        display_error("No PDC with for id : " + idP);
-                    } else {
-                        console.log('Got PDC: ', rows[0].id_pdc);
-                        var pdc_id = rows[0].id_pdc;
-                        var ronde_id = rows[0].id_ronde;
-
-                        // Récupérer l'instance de ronde concernée par le pdc pour la date données
-                        connection.query('SELECT rxd.id_ronde_x_date FROM rondeXDate rxd INNER JOIN ronde r ON r.id_ronde = rxd.id_ronde WHERE rxd.id_ronde = ? AND rxd.date_ronde = ? AND r.id_garde = ?', [ronde_id, timeP, garde_id], function(err, rows, fields) {
-                            if (err) throw err;
+                    // Vérifier que l'id du PDC existe.
+                    connection.query('SELECT pdc.id_pdc, pdc.id_ronde FROM pointDeControle pdc WHERE pdc.id_pdc = ?', [idP], function(err, rows, fields) {
+                        if (err) display_error(err, connection);
+                        else {
                             if (!rows || rows.length == 0) {
-                                display_error("No ronde with for id : " + ronde_id + " and time "+ timeP);
+                                display_error("No PDC with for id : " + idP);
                             } else {
-                                console.log('Got Garde: ', rows[0].id_pdc);
-                                var ronde_inst_id = rows[0].id_ronde_x_date;
+                                console.log('Got PDC: ', rows[0].id_pdc);
+                                var pdc_id = rows[0].id_pdc;
+                                var ronde_id = rows[0].id_ronde;
 
-                                connection.query('INSERT INTO badgeage(id_garde, id_pdc, id_ronde_x_date, date_badgeage) VALUES(?, ?, ?, ?)', [garde_id, pdc_id, ronde_inst_id, timeP], function(err, rows, fields) {
-                                    if (err) throw err;
-                                    res.render('api', {
-                                        title: 'SigFox Callback API',
-                                        id: idP,
-                                        data: dataP,
-                                        time: timeP.toLocaleDateString(),
-                                        error: null
-                                    });
-                                    connection.end();
-                                    // Check update OK
+                                // Récupérer l'instance de ronde concernée par le pdc pour la date données
+                                connection.query('SELECT rxd.id_ronde_x_date FROM rondeXDate rxd INNER JOIN ronde r ON r.id_ronde = rxd.id_ronde WHERE rxd.id_ronde = ? AND rxd.date_ronde = ? AND r.id_garde = ?', [ronde_id, timeP, garde_id], function(err, rows, fields) {
+                                    if (err) display_error(err, connection);
+                                    else {
+                                        if (!rows || rows.length == 0) {
+                                            display_error("No ronde with for id : " + ronde_id + " and time " + timeP.toLocaleDateString());
+                                        } else {
+                                            console.log('Got ronde instance with id : ', rows[0].id_ronde_x_date);
+                                            var ronde_inst_id = rows[0].id_ronde_x_date;
+
+                                            connection.query('INSERT INTO badgeage(id_garde, id_pdc, id_ronde_x_date, date_badgeage) VALUES(?, ?, ?, ?)', [garde_id, pdc_id, ronde_inst_id, timeP], function(err, rows, fields) {
+                                                if (err) {
+                                                  if (err.code && err.code == 'ER_DUP_ENTRY') {
+                                                    display_error("This Badgeage already exists.", connection);
+                                                  } else {
+                                                    display_error(err, connection);
+                                                  }
+                                                } else {
+                                                    res.render('api', {
+                                                        title: 'SigFox Callback API',
+                                                        id: idP,
+                                                        data: dataP,
+                                                        time: timeP.toLocaleDateString(),
+                                                        error: null
+                                                    });
+                                                    connection.end();
+                                                    // Check update OK
+                                                }
+                                            });
+                                        }
+                                    }
                                 });
                             }
-                        });
-                    }
-                });
+                        }
+                    });
+                }
             }
         });
 
-} catch (ex) {
-  display_error(ex);
-  connection.end();
-}
+
 
     } else {
         display_error("Invalid parameters")
